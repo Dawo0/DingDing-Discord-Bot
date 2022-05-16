846401e1ea3651c5d63891b15ad59ec791000b5d
Dawoo <104740901+Dawo0@users.noreply.github.com>
2022-05-17 00:52:13 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# DingDing-Discord-Bot
+# Ding Ding Discord Bot
+
 This Discord bot will search for existing text channels and post given string or will create a new channel with the given name and post the string on this specific channel.
+
+## Badges
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+## Documentation
+
+!help | Displays All Availble Commands
+
+!post <name> <string> | Searches if name exists as a text-channel it sends string else it creates a text channel and sends string
+
+!say <msg> | It deletes ctx.command and bot sends the given msg
---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+
+description = "!post <name> <links>"
+bot = commands.Bot(command_prefix='!', description=description)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command()
+async def post(ctx, name: str, *, links: str):
+    '''takes name argument checks if channel exists and sends links to channel'''
+    
+    try:
+        channel = discord.utils.get(ctx.guild.channels, name=name)
+        channel_id = channel.id
+        specific_write = bot.get_channel(channel_id)
+        await specific_write.send(links)
+    
+    except:
+        guild = ctx.guild
+        cat = 'models'
+        category = discord.utils.get(ctx.guild.categories, name=cat)
+        create_channel = await guild.create_text_channel(name, category=category)
+        channel = discord.utils.get(ctx.guild.channels, name=name)
+        channel_id = channel.id
+        specific_write = bot.get_channel(channel_id)
+        await specific_write.send(links)
+
+    await ctx.message.delete()
+
+
+@bot.command()
+async def say(ctx, *, msg: str):
+    await ctx.send(msg)
+    await ctx.message.delete()
+
+
+bot.run("OTc1ODMzMjUyOTYxMDgzNTEy.GCy3T7.gxMkwX1VV3HiaQ6roWDcx9qQWo_iIXUICr6LLs")
